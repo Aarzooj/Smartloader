17abccc3ed003535986b2d79b980e43118df4d2d
Aarzoo Jhamb <aarzoo22008@iiitd.ac.in>
2023-10-27 01:14:25 +0530

    a

--- sum.c
+++ sum.c
@@ -1,12 +1,9 @@
-/*
- * No changes are allowed in this file
- */
-int fib(int n) {
-  if(n<2) return n;
-  else return fib(n-1)+fib(n-2);
-}
-
+#define SIZE 1024
+int __attribute__((aligned(4096))) A[SIZE] = { 0 };
+int sum = 0;
 int _start() {
-	int val = fib(40);
-	return val;
+  for (int i = 0; i < SIZE; i++) A[i] = 2;
+  for (int i = 0; i < SIZE; i++)
+    sum += A[i];
+  return sum;
 }
